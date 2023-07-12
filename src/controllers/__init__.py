7790be9c034c09ed7731319a1db0fdadbb52ad02
Victor Koji <vitorkoji15@gmail.com>
2023-07-11 23:10:05 -0300

    feat: Add crud stand

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from controllers import (
     product_controller, product_category_controller, user_controller, auth_controller, event_controller,
-    stand_category_controller
+    stand_category_controller, stand_controller
 )
 
 private_api_router = APIRouter()
@@ -11,6 +11,7 @@ private_api_router.include_router(product_category_controller.router)
 private_api_router.include_router(user_controller.router)
 private_api_router.include_router(event_controller.router)
 private_api_router.include_router(stand_category_controller.router)
+private_api_router.include_router(stand_controller.router)
 
 public_api_router = APIRouter()
 public_api_router.include_router(auth_controller.router)
---
+++ src/controllers/stand_controller.py
@@ -0,0 +1,60 @@
+from fastapi import APIRouter, Response, status
+from services.stand_service import StandService
+from schemas.stand_schema import StandCreateSchema, StandSchema
+from fastapi.responses import JSONResponse
+from typing import List
+from exceptions.custom_exception import DatabaseError
+
+stand_service = StandService()
+router = APIRouter(
+    prefix="/stands",
+    tags=["stands"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get('/', response_model=List[StandSchema])
+def get_all_items():
+    try:
+        stands = stand_service.get()
+        return stands.serialize()
+    except Exception as ex:
+        return JSONResponse(content={'message': ex.message}, status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get('/{stand_id}', response_model=StandSchema)
+def get_stand(stand_id: int):
+    try:
+        stand = stand_service.get(stand_id)
+        if (stand):
+            return stand.serialize()
+        else:
+            return JSONResponse(content={'message': 'Stand not found!'}, status_code=status.HTTP_404_NOT_FOUND)
+    except Exception as ex:
+        return JSONResponse(content={'message': ex.message}, status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.post('/', response_model=StandSchema, status_code=status.HTTP_201_CREATED)
+def add_stand(stand: StandCreateSchema):
+    try:
+        stand = stand_service.add(stand.dict())
+        return stand.serialize()
+    except DatabaseError as ex:
+        return JSONResponse(content={'message': ex.message}, status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.put('/', response_model=StandSchema)
+def update_stand(stand: StandSchema):
+    try:
+        stand = stand_service.update(stand.dict())
+        return stand.serialize()
+    except DatabaseError as ex:
+        return JSONResponse(content={'message': ex.message}, status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.delete('/{stand_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+def delete_stand(stand_id):
+    try:
+        stand_service.delete(stand_id)
+    except DatabaseError as ex:
+        return JSONResponse(content={'message': ex.message}, status_code=status.HTTP_400_BAD_REQUEST)
